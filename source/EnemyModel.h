f4bb0db934a319685a0ed6ee22456d78d2714468
Joshua Chan <jdc345@cornell.edu>
2018-03-15 15:54:39 -0400

    enemyModel preliminary

---
+++ source/EnemyModel.h
@@ -0,0 +1,54 @@
+//
+// EnemyModel.h
+//
+// Created by Josh on 3/14/2018.
+//
+
+#ifndef BUILD_ANDROID_ENEMYMODEL_H
+#define BUILD_ANDROID_ENEMYMODEL_H
+
+#include <cugl/cugl.h>
+
+class EnemyModel : public cugl::BoxObstacle{
+private:
+    //always do this?
+    CU_DISALLOW_COPY_AND_ASSIGN(EnemyModel);
+
+public:
+
+    //constructors
+    EnemyModel(void) : BoxObstacle() { }
+
+    static std::shared_ptr<EnemyModel> alloc(cugl::Vec2 pos, float dir, int type, int drawScale,
+                                             const std::shared_ptr<cugl::AssetManager>& assets) {
+        std::shared_ptr<EnemyModel> ref = std::make_shared<EnemyModel>();
+        return (ref->init(pos, dir, type, drawScale, assets) ? ref : nullptr);
+    }
+
+    bool init(cugl::Vec2 pos, float dir, int type, int drawScale, const std::shared_ptr<cugl::AssetManager>& assets);
+
+
+    //methods
+    void update(float deltaTime) override;
+
+    // Assume assets are already loaded, and _node is immutable after init
+    const std::shared_ptr<cugl::PolygonNode> getNode() const { return _node; }
+
+
+    //destructors
+    void dispose();
+
+    ~EnemyModel() {dispose();}
+
+
+protected:
+    //private
+
+    //This is the root scene node that corresponds to this model.
+    std::shared_ptr<cugl::PolygonNode> _node;
+
+	//pixels per meter measurement. obstacle world * _drawScale = screen size
+	int _drawScale;
+};
+
+#endif //BUILD_ANDROID_ENEMYMODEL_H
