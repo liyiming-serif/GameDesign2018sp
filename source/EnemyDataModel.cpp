72aaff89f5234da5d4d8381de6baac32192dbb6f
Yiming Li <liyiming.serif@gmail.com>
2018-04-17 21:31:46 -0400

    add EnemyDataModel.cpp shell for android studio

---
+++ source/EnemyDataModel.cpp
@@ -0,0 +1,12 @@
+// EnemyDataModel.cpp
+// EnemyDataModel shell just so Android studio will play nicely.
+//
+//
+// Created by Yiming on 4/17/2018.
+//
+
+#include "EnemyDataModel.h"
+
+
+using namespace cugl;
+
